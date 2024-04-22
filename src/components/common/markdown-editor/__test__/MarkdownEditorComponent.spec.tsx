81673a3da93b676997fbcd7b333ebd88ae9a5167
pascallinder <pascal.linder97@hotmail.com>
2024-04-22 13:47:08 +0200

    fixes

---
+++ src/components/common/markdown-editor/__test__/MarkdownEditorComponent.spec.tsx
@@ -0,0 +1,16 @@
+import { describe, expect, test } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import MarkdownEditorComponent from '../MarkdownEditorComponent';
+import React from 'react';
+
+describe('MarkdownEditor test', () => {
+  test('Should show title', () => {
+    const random = Math.random();
+    render(
+      <MarkdownEditorComponent
+        src={String(random)}
+        onChange={() => {}}></MarkdownEditorComponent>,
+    );
+    expect(screen.getByText(new RegExp(`${random}`, 'i'))).toBeDefined();
+  });
+});
